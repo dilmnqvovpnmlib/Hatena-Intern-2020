a8d4eadcaeecf5a9800e6d73aef61d7e29f32a53
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-09-08 01:34:27 +0900

    do k8s-handson

--- services/renderer-ts/src/renderer.ts
+++ services/renderer-ts/src/renderer.ts
@@ -1,7 +1,7 @@
 /**
  * 受け取った文書を HTML に変換する
  */
-export async function render(src: string): Promise<string> {
+export async function render(src : string): Promise<string> {
   // TODO: これはサンプル実装 (URL の自動リンク) です
   const html = src.replace(/https?:\/\/[^\s]+/g, (url) => `<a href="${encodeURI(url)}">${url}</a>`);
   return html;
