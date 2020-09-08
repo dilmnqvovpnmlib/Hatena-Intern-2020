eeb4538f3a44db3c1e1a7e0b5cde9914d5efbacc
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-09-08 15:33:15 +0900

    #1 create test code

--- services/renderer-go/renderer/renderer.go
+++ services/renderer-go/renderer/renderer.go
@@ -1,32 +1,62 @@
 package renderer
-
 import (
-	"bytes"
-	"context"
-	"html/template"
-	"regexp"
+    "fmt"
+    "bytes"
+    "context"
+    "html/template"
+    "regexp"
 	"github.com/yuin/goldmark"
+    // "github.com/yuin/goldmark/extension"
+    // "github.com/yuin/goldmark/parser"
+	// "github.com/yuin/goldmark/renderer/html"
+	// "github.com/yuin/goldmark-highlighting"
 )
 
 var urlRE = regexp.MustCompile(`https?://[^\s]+`)
 var linkTmpl = template.Must(template.New("link").Parse(`<a href="{{.}}">{{.}}</a>`))
 
+func ParseHtml(src string) (string, error) {
+	// md := goldmark.New(
+	// 	goldmark.WithExtensions(extension.GFM),
+	// 	goldmark.WithParserOptions(
+	// 		parser.WithAutoHeadingID(),
+	// 	),
+	// 	goldmark.WithRendererOptions(
+	// 		html.WithHardWraps(),
+	// 		html.WithXHTML(),
+	// 	),
+	// )
+    fmt.Println("------------------markdown-----------------------------------")
+	// markdown := goldmark.New(
+	// 	goldmark.WithExtensions(
+	// 		Highlighting,
+	// 	),
+	// )
+    var buf bytes.Buffer
+    if err := goldmark.Convert([]byte(src), &buf); err != nil {
+        panic(err)
+    }
+    html := buf.String()
+    return html, nil
+}
+
 // Render は受け取った文書を HTML に変換する
 func Render(ctx context.Context, src string) (string, error) {
-	// TODO: これはサンプル実装 (URL の自動リンク) です
-	// もとからあったコード
-	// html := urlRE.ReplaceAllStringFunc(src, func(url string) string {
-	// 	var w bytes.Buffer
-	// 	err := linkTmpl.ExecuteTemplate(&w, "link", url)
-	// 	if err != nil {
-	// 		return url
-	// 	}
-	// 	return w.String()
-	// })
-	var buf bytes.Buffer
-	if err := goldmark.Convert([]byte(src), &buf); err != nil {
-		panic(err)
-	}
-	html := buf.String()
-	return html, nil
+    // TODO: これはサンプル実装 (URL の自動リンク) です
+    // もとからあったコード
+    // html := urlRE.ReplaceAllStringFunc(src, func(url string) string {
+    //  var w bytes.Buffer
+    //  err := linkTmpl.ExecuteTemplate(&w, "link", url)
+    //  if err != nil {
+    //      return url
+    //  }
+    //  return w.String()
+    // })
+    fmt.Println("------------------Hello0000-----------------------------------")
+    // var buf bytes.Buffer
+    // if err := goldmark.Convert([]byte(src), &buf); err != nil {
+    //  panic(err)
+    // }
+    html, err := ParseHtml(src) // buf.String()
+    return html, err
 }
--- services/renderer-go/renderer/renderer_test.go
+++ services/renderer-go/renderer/renderer_test.go
@@ -1,15 +1,33 @@
 package renderer
 
 import (
-	"context"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
+    "fmt"
+    "context"
+    "testing"
+    "github.com/stretchr/testify/assert"
 )
 
 func Test_Render(t *testing.T) {
-	src := `foo https://google.com/ bar`
-	html, err := Render(context.Background(), src)
-	assert.NoError(t, err)
-	assert.Equal(t, `foo <a href="https://google.com/">https://google.com/</a> bar`, html)
+    // src := `foo https://google.com/ bar`
+    // html, err := Render(context.Background(), src)
+    // assert.NoError(t, err)
+	// assert.Equal(t, `foo <a href="https://google.com/">https://google.com/</a> bar`, html)
+	fmt.Println("---------Test----------")
+    // 記法のテスト
+    headlineSrc := `# 見出し`
+    headlineHtml, headlineErr := Render(context.Background(), headlineSrc)
+    assert.NoError(t, headlineErr)
+	assert.Equal(t, "<h1>見出し</h1>\n", headlineHtml)
+    // リストのテスト
+    listSrc := "- リスト"
+    listHtml, listErr := Render(context.Background(), listSrc)
+    assert.NoError(t, listErr)
+	assert.Equal(t, "<ul>\n<li>リスト</li>\n</ul>\n", listHtml)
+    // リンクのテスト
+    linkSrc := "[localhost](localhost:8000)"
+    linkHtml, linkErr := Render(context.Background(), linkSrc)
+    assert.NoError(t, linkErr)
+	assert.Equal(t, `<p><a href="localhost:8000">localhost</a></p>` + "\n", linkHtml)
+	// 独自記法
+	fmt.Println("---------Test----------")
 }
