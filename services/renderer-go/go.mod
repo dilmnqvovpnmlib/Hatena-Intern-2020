36198393eb221a7a33c5d41b8974c40e9b4687e5
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-09-08 17:11:07 +0900

    #1 create original loginc and test code

--- services/renderer-go/go.mod
+++ services/renderer-go/go.mod
@@ -3,10 +3,11 @@ module github.com/hatena/Hatena-Intern-2020/services/renderer-go
 go 1.14
 
 require (
-    github.com/yuin/goldmark v1.2.1
 	github.com/golang/protobuf v1.4.1
 	github.com/grpc-ecosystem/go-grpc-middleware v1.2.0
 	github.com/stretchr/testify v1.4.0
+	github.com/yuin/goldmark v1.2.1
+	github.com/yuin/goldmark-highlighting v0.0.0-20200307114337-60d527fdb691
 	go.uber.org/zap v1.15.0
 	google.golang.org/grpc v1.30.0
 	google.golang.org/protobuf v1.25.0
--- services/renderer-go/renderer/renderer_test.go
+++ services/renderer-go/renderer/renderer_test.go
@@ -1,33 +1,41 @@
 package renderer
 
 import (
-    "fmt"
+	"fmt"
     "context"
     "testing"
     "github.com/stretchr/testify/assert"
 )
 
 func Test_Render(t *testing.T) {
-    // src := `foo https://google.com/ bar`
-    // html, err := Render(context.Background(), src)
-    // assert.NoError(t, err)
-	// assert.Equal(t, `foo <a href="https://google.com/">https://google.com/</a> bar`, html)
-	fmt.Println("---------Test----------")
-    // 記法のテスト
+	// 記法のテスト
     headlineSrc := `# 見出し`
     headlineHtml, headlineErr := Render(context.Background(), headlineSrc)
     assert.NoError(t, headlineErr)
 	assert.Equal(t, "<h1>見出し</h1>\n", headlineHtml)
+
+	fmt.Println("------------------------------")
+
     // リストのテスト
     listSrc := "- リスト"
     listHtml, listErr := Render(context.Background(), listSrc)
     assert.NoError(t, listErr)
 	assert.Equal(t, "<ul>\n<li>リスト</li>\n</ul>\n", listHtml)
+
+	fmt.Println("------------------------------")
+
     // リンクのテスト
     linkSrc := "[localhost](localhost:8000)"
     linkHtml, linkErr := Render(context.Background(), linkSrc)
     assert.NoError(t, linkErr)
 	assert.Equal(t, `<p><a href="localhost:8000">localhost</a></p>` + "\n", linkHtml)
+
+	fmt.Println("------------------------------")
+
 	// 独自記法
-	fmt.Println("---------Test----------")
+	originalSrc := "``` bash\n" + "echo Hello" + "\n" + "```"
+	originalHtml, originalErr := Render(context.Background(), originalSrc)
+	ans := `<pre style="background-color:#fff"><span style="color:#0086b3">echo</span> Hello` + "\n" + "</pre>"
+    assert.NoError(t, originalErr)
+	assert.Equal(t, ans, originalHtml)
 }
