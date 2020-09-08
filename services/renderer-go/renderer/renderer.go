998e110f892a6ce42b1ef04234736684afb63b73
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-09-08 17:18:30 +0900

    #1 format

--- services/renderer-go/renderer/renderer.go
+++ services/renderer-go/renderer/renderer.go
@@ -1,32 +1,33 @@
 package renderer
+
 import (
-    "bytes"
-    "context"
-    "html/template"
-    "regexp"
+	"bytes"
+	"context"
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark-highlighting"
+	"html/template"
+	"regexp"
 )
 
 var urlRE = regexp.MustCompile(`https?://[^\s]+`)
 var linkTmpl = template.Must(template.New("link").Parse(`<a href="{{.}}">{{.}}</a>`))
 
 func ParseHtml(src string) (string, error) {
-    markdown := goldmark.New(
-        goldmark.WithExtensions(
-            highlighting.Highlighting,
-        ),
-    )
-    var buf bytes.Buffer
-    if err := markdown.Convert([]byte(src), &buf); err != nil {
-        panic(err)
-    }
-    html := buf.String()
-    return html, nil
+	markdown := goldmark.New(
+		goldmark.WithExtensions(
+			highlighting.Highlighting,
+		),
+	)
+	var buf bytes.Buffer
+	if err := markdown.Convert([]byte(src), &buf); err != nil {
+		panic(err)
+	}
+	html := buf.String()
+	return html, nil
 }
 
 // Render は受け取った文書を HTML に変換する
 func Render(ctx context.Context, src string) (string, error) {
-    html, err := ParseHtml(src) // buf.String()
-    return html, err
+	html, err := ParseHtml(src) // buf.String()
+	return html, err
 }
--- services/renderer-go/renderer/renderer_test.go
+++ services/renderer-go/renderer/renderer_test.go
@@ -1,34 +1,34 @@
 package renderer
 
 import (
+	"context"
 	"fmt"
-    "context"
-    "testing"
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func Test_Render(t *testing.T) {
 	// 記法のテスト
-    headlineSrc := `# 見出し`
-    headlineHtml, headlineErr := Render(context.Background(), headlineSrc)
-    assert.NoError(t, headlineErr)
+	headlineSrc := `# 見出し`
+	headlineHtml, headlineErr := Render(context.Background(), headlineSrc)
+	assert.NoError(t, headlineErr)
 	assert.Equal(t, "<h1>見出し</h1>\n", headlineHtml)
 
 	fmt.Println("------------------------------")
 
-    // リストのテスト
-    listSrc := "- リスト"
-    listHtml, listErr := Render(context.Background(), listSrc)
-    assert.NoError(t, listErr)
+	// リストのテスト
+	listSrc := "- リスト"
+	listHtml, listErr := Render(context.Background(), listSrc)
+	assert.NoError(t, listErr)
 	assert.Equal(t, "<ul>\n<li>リスト</li>\n</ul>\n", listHtml)
 
 	fmt.Println("------------------------------")
 
-    // リンクのテスト
-    linkSrc := "[localhost](localhost:8000)"
-    linkHtml, linkErr := Render(context.Background(), linkSrc)
-    assert.NoError(t, linkErr)
-	assert.Equal(t, `<p><a href="localhost:8000">localhost</a></p>` + "\n", linkHtml)
+	// リンクのテスト
+	linkSrc := "[localhost](localhost:8000)"
+	linkHtml, linkErr := Render(context.Background(), linkSrc)
+	assert.NoError(t, linkErr)
+	assert.Equal(t, `<p><a href="localhost:8000">localhost</a></p>`+"\n", linkHtml)
 
 	fmt.Println("------------------------------")
 
@@ -36,6 +36,6 @@ func Test_Render(t *testing.T) {
 	originalSrc := "``` bash\n" + "echo Hello" + "\n" + "```"
 	originalHtml, originalErr := Render(context.Background(), originalSrc)
 	ans := `<pre style="background-color:#fff"><span style="color:#0086b3">echo</span> Hello` + "\n" + "</pre>"
-    assert.NoError(t, originalErr)
+	assert.NoError(t, originalErr)
 	assert.Equal(t, ans, originalHtml)
 }
