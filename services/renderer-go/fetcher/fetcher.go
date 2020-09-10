d2c6a6c876f6399cdaa78f5dbeff461e66e7f44a
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-09-10 12:19:30 +0900

    #2 refactoring in renderer/renderer.go

--- services/renderer-go/fetcher/fetcher.go
+++ services/renderer-go/fetcher/fetcher.go
@@ -1,55 +1,16 @@
 package fetcher
 
 import (
-	"bytes"
-	"fmt"
+	"context"
 	"log"
 
-	"github.com/yuin/goldmark"
-	"github.com/yuin/goldmark/ast"
-	"github.com/yuin/goldmark/parser"
-	"github.com/yuin/goldmark/text"
-	"github.com/yuin/goldmark/util"
 	pb_fetcher "github.com/dilmnqvovpnmlib/Hatena-Intern-2020/services/renderer-go/pb/fetcher"
 )
 
-var markdown = goldmark.New(
-	goldmark.WithParserOptions(
-		parser.WithASTTransformers(
-			util.Prioritized(&autoTitleLinker{}, 999),
-		),
-	),
-)
-
-func main() {
-	src := []byte("# link samples\n" +
-		"[normal link](https://example.com)\n" +
-		"[](https://example.com)\n")
-	var buf bytes.Buffer
-	if err := markdown.Convert(src, &buf); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", buf.String())
-}
-
-type autoTitleLinker struct {
-	fetcherCli pb_fetcher.FetcherClient
-}
-
-func (l *autoTitleLinker) Transform(node *ast.Document, reader text.Reader, pc parser.Context) {
-	ast.Walk(node, func(node ast.Node, entering bool) (ast.WalkStatus, error) {
-		if node, ok := node.(*ast.Link); ok && entering && node.ChildCount() == 0 {
-			node.AppendChild(node, ast.NewString([]byte(fetchTitle(l.fetcherCli, string(node.Destination)))))
-		}
-		return ast.WalkContinue, nil
-	})
-}
-
-// grpc で処理する部分に相当してて、 /app/app.go の Render のこと
-func fetchTitle(fetcherCli pb_fetcher.FetcherClient, url string) string {
-	reply, err := fetcherCli.Render(ctx, &pb_fetcher.FetcherRequest{Url: url})
+func FetchTitle(ctx context.Context, url string, fetcherClient pb_fetcher.FetcherClient) string {
+	reply, err := fetcherClient.Render(ctx, &pb_fetcher.FetcherRequest{Url: url})
 	if err != nil {
-		return nil
+		log.Fatal(err)
 	}
 	return reply.Title
 }
--- services/renderer-go/renderer/renderer.go
+++ services/renderer-go/renderer/renderer.go
@@ -3,7 +3,6 @@ package renderer
 import (
 	"bytes"
 	"context"
-	"log"
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark-highlighting"
 	"github.com/yuin/goldmark/ast"
@@ -11,29 +10,22 @@ import (
 	"github.com/yuin/goldmark/text"
 	"github.com/yuin/goldmark/util"
 
+	"github.com/dilmnqvovpnmlib/Hatena-Intern-2020/services/renderer-go/fetcher"
 	pb_fetcher "github.com/dilmnqvovpnmlib/Hatena-Intern-2020/services/renderer-go/pb/fetcher"
 )
 
 type autoTitleLinker struct {
-	ctx context.Context
+	ctx           context.Context
 	fetcherClient pb_fetcher.FetcherClient
 }
 
 func (l *autoTitleLinker) Transform(node *ast.Document, reader text.Reader, pc parser.Context) {
 	ast.Walk(node, func(node ast.Node, entering bool) (ast.WalkStatus, error) {
 		if node, ok := node.(*ast.Link); ok && entering && node.ChildCount() == 0 {
-			node.AppendChild(node, ast.NewString([]byte(fetchTitle(l.ctx, string(node.Destination), l.fetcherClient))))
+			node.AppendChild(node, ast.NewString([]byte(fetcher.FetchTitle(l.ctx, string(node.Destination), l.fetcherClient))))
 		}
 		return ast.WalkContinue, nil
 	})
-}
-
-func fetchTitle(ctx context.Context, url string, fetcherClient pb_fetcher.FetcherClient) string {
-	reply, err := fetcherClient.Render(ctx, &pb_fetcher.FetcherRequest{Url: url})
-	if err != nil {
-		log.Fatal(err)
-	}
-	return reply.Title
 }
 
 func ParseHtml(ctx context.Context, src string, fetcherClient pb_fetcher.FetcherClient) (string, error) {
